eef7cdff56027fa5f635a3585310110bd1098c1e
Joe Newbry <joenewbry@gmail.com>
2012-12-04 11:04:39 -0800

    Revert "deploy to heroku"
    This reverts commit 3b24b3648dcdfb192a9f540d7728e517d1563ccc.

--- testsite/settings.py
+++ testsite/settings.py
@@ -17,14 +17,6 @@ DATABASES = {
         'NAME': 'grubcrew',                      # Or path to database file if using sqlite3.
         'USER': 'joe',                      # Not used with sqlite3.
         'PASSWORD': 'freba21',                  # Not used with sqlite3.
-        'HOST': 'powerful-bayou-4139.herokuapp.com',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    } ,
-'local': {    
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'grubcrew',                      # Or path to database file if using sqlite3.
-        'USER': 'joe',                      # Not used with sqlite3.
-        'PASSWORD': 'freba21',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     } ,
